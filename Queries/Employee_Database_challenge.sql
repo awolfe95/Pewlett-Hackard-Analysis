48e6a8fd39aad76e5d1b4faee20e0736bcd857cc
awolfe95 <82780266+awolfe95@users.noreply.github.com>
2021-06-12 12:35:42 -0400

    adding final challenge query

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,60 @@
+-- Retrieve retiring employees and their titles
+Select e.emp_no, 
+e.first_name, 
+e.last_name, 
+t.title, 
+t.from_date, 
+t.to_date
+Into retirement_titles
+From employees as e
+Inner join titles as t
+On (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+Order by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) 
+rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+--Number of retiring employees by most recent title
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count desc;
+
+--Retrieve employees eligible for mentorship program
+Select Distinct on (e.emp_no)
+e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date, 
+de.from_date, 
+de.to_date, 
+t.title
+Into mentorship_eligibility
+From employees as e
+Inner join dept_emp as de
+On (e.emp_no = de.emp_no)
+Inner join titles as t
+On (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+-- Retrieve count of number of roles that will need to be filled
+SELECT Sum (rt.count)
+INTO retiring_count
+FROM retiring_titles as rt;
+
+-- Retrieve count of mentorship eligible employees
+Select count (me.emp_no)
+INTO mentorship_count
+From mentorship_eligibility as me;
+
